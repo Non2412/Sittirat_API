f1ec1f08fb134c82119f78b1384b0999e1a22015
Non2412 <ctf2547@gmail.com>
2025-10-03 10:30:00 +0700

    update settings.py MIDDKEWARE

--- backend/settings.py
+++ backend/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # เพิ่มสำหรับ CORS
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add whitenoise for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
